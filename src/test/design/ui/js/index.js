27df63b7cce03af315a054b28b898330c620f4d7
Laurens van Eekhout <eekhout.l@tkppensioen.nl>
2017-10-17 16:48:14 +0200

    Meer dynamiek

--- src/test/design/ui/js/index.js
+++ src/test/design/ui/js/index.js
@@ -1,16 +1,27 @@
-function getJson(callback) {
+function getRekeningoverzichtJson(opened, done) {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) callback(this.response);
+        console.log(this.readyState);
+        if (this.readyState == 1) opened();
+        else if (this.readyState == 4) {
+            if (this.status>=200&this.status<300)
+                done(JSON.parse(this.responseText));
+        }
     }
-    xhttp.open("GET", "http://localhost");
+    xhttp.open("GET", "json/categorie.json");
     xhttp.send();
 }
 
 function appendCategories(el, categories) {
-    categories.forEach(value => el.innerHTML += categorieTemplate.innerHTML.replace("{{categorienaam}}", value));
+    categories.vast.forEach(value => el.innerHTML += categorieTemplate.innerHTML.replace("{{categorienaam}}", value.categorie));
+    categories.incidenteel.forEach(value => el.innerHTML += categorieTemplate.innerHTML.replace("{{categorienaam}}", value.categorie));
 }
 
 window.onload = () => {
-    appendCategories(document.body, ["Boodschappen","Tanken"]);
+    let ul = document.getElementById("rekeningoverzicht");
+    if (!ul||ul.constructor.name!="HTMLUListElement") throw new Error("HTML element ul.rekeningoverzicht niet gevonden");
+
+    getRekeningoverzichtJson(
+        () => ul.innerHTML = "",
+        done => appendCategories(ul, done));
 }
