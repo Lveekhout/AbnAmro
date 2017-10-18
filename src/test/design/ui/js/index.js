6edb585749a14ba18e089433de90eeaeacaab088
Laurens van Eekhout <eekhout.l@tkppensioen.nl>
2017-10-18 18:26:48 +0200

    Create elements

--- src/test/design/ui/js/index.js
+++ src/test/design/ui/js/index.js
@@ -1,15 +1,74 @@
-function getRekeningoverzichtJson(opened, done) {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        console.log(this.readyState);
-        if (this.readyState == 1) opened();
-        else if (this.readyState == 4) {
-            if (this.status>=200&this.status<300)
-                done(JSON.parse(this.responseText));
-        }
-    }
-    xhttp.open("GET", "json/categorie.json");
-    xhttp.send();
+function createExpandknopAnchorImage() {
+    let img = document.createElement("img");
+    img.src="images/plus.png"
+    return img;
+}
+
+function createExpandknopAnchor() {
+    let a = document.createElement("a");
+    a.href = "javascript: void(0);";
+    a.addEventListener("click", event => {
+        if (a.parentNode.parentNode.nextElementSibling)
+            a.parentNode.parentNode.nextElementSibling.style.display = a.parentNode.parentNode.nextElementSibling.style.display == "" ? "block" : ""
+    });
+    a.onclick = "toggleElement(this.parentNode.parentNode.nextElementSibling)";
+    a.appendChild(createExpandknopAnchorImage())
+    return a;
+}
+
+function createExpandknop() {
+    let div = document.createElement("div");
+    div.classList.add("expandknop");
+    div.appendChild(createExpandknopAnchor());
+    return div;
+}
+
+function createTekst(tekst) {
+    let div = document.createElement("div");
+    div.classList.add("tekst");
+    div.textContent = tekst;
+    return div;
+}
+
+function createTextcontainer(post) {
+    let div = document.createElement("div");
+    div.classList.add("textcontainer");
+    div.appendChild(createExpandknop());
+    div.appendChild(createTekst(post.categorie));
+    return div;
+}
+
+function createNamenTable(namen) {
+    let table = document.createElement("table");
+    namen.forEach(naam => {
+        let tr = document.createElement("tr");
+        let td = document.createElement("td");
+        td.appendChild(document.createTextNode(naam.naam));
+        tr.appendChild(td);
+        td = document.createElement("td");
+        td.appendChild(document.createTextNode(naam.bedrag.credit));
+        tr.appendChild(td);
+        td = document.createElement("td");
+        td.appendChild(document.createTextNode(naam.bedrag.debet));
+        tr.appendChild(td);
+        table.appendChild(tr);
+    });
+    return table;
+}
+
+function createNamen(namen) {
+    let div = document.createElement("div");
+    div.classList.add("namen");
+    div.appendChild(createNamenTable(namen))
+    return div;
+}
+
+function createCategorie(categorie) {
+    let div = document.createElement("div");
+    div.classList.add("categorie");
+    div.appendChild(createTextcontainer(categorie.incidenteel[0]));
+    div.appendChild(createNamen(categorie.incidenteel[0].namen));
+    return div;
 }
 
 function appendCategories(el, categories) {
@@ -18,10 +77,17 @@ function appendCategories(el, categories) {
 }
 
 window.onload = () => {
-    let ul = document.getElementById("rekeningoverzicht");
-    if (!ul||ul.constructor.name!="HTMLUListElement") throw new Error("HTML element ul.rekeningoverzicht niet gevonden");
+    let div = document.getElementById("rekeningoverzicht");
+    if (!div||div.constructor.name!="HTMLDivElement") throw new Error("HTML element div.rekeningoverzicht niet gevonden");
 
-    getRekeningoverzichtJson(
-        () => ul.innerHTML = "",
-        done => appendCategories(ul, done));
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 1)
+            div.innerHTML = "";
+        else if (this.readyState == 4)
+            if (this.status>=200&this.status<300)
+                div.appendChild(createCategorie(JSON.parse(this.responseText)))
+    }
+    xhttp.open("GET", "json/categorie.json");
+    xhttp.send();
 }
